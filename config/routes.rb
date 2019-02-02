a8ce35d0d0536677359185a858128c8e6a67acda
283kkc <pictures1682@gmail.com>
2019-02-02 11:49:14 +0000

    Initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'pages#index'
+  get 'pages/show'
     get  'login' => 'login#index'
     get  'login/new'  =>  'login#new'
     get  'user' => 'user#index'

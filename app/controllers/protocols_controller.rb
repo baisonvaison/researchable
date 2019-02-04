af40dc1b09e3f6a0c6890287b2277a83b0845b20
take77 <teke77@toki.waseda.jp>
2019-02-05 01:33:29 +0900

    feat: create protocol function (before introduce Ajax)

--- app/controllers/protocols_controller.rb
+++ app/controllers/protocols_controller.rb
@@ -1,11 +1,15 @@
 class ProtocolsController < ApplicationController
+<<<<<<< HEAD
 
+=======
+>>>>>>> feat: create protocol function (before introduce Ajax)
   def index
   end
 
   def show
     protocol_id = params[:id]
     @protocol = Protocol.find(protocol_id)
+<<<<<<< HEAD
     @produces = Procedures.where(protocol_id: protocol_id)
   end
 
@@ -37,6 +41,11 @@ class ProtocolsController < ApplicationController
   private
   def create_params
     params.require(:protocol).permit(:title,  procedures_attributes: [:text, :_destroy])
+=======
+  end
+
+  def create
+>>>>>>> feat: create protocol function (before introduce Ajax)
   end
 
 end

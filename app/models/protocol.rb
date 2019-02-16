68d5f239e24575365d40360e48f86af937d3a5a0
take77 <teke77@toki.waseda.jp>
2019-02-17 01:53:09 +0900

    feat: create protocol function only template

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -1,6 +1,6 @@
 class Protocol < ApplicationRecord
-  belongs_to :user
-  belongs_to :affiliation
+  # belongs_to :user
+  # belongs_to :affiliation
   has_many :procedures, inverse_of: :protocol
   has_many :experiments
 
@@ -12,4 +12,16 @@ class Protocol < ApplicationRecord
   BASE = 0
   CUSTOM = 1
   TEMPLATE =2
+
+  # add position to procedures
+  after_save do |protocol|
+    procedures = Procedure.where(protocol_id: protocol.id)
+    add_position = 0
+    procedures.each do |procedure|
+      add_position += 1
+      procedure.position = add_position
+      procedure.save!
+    end
+  end
+
 end

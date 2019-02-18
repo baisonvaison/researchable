893b03535a6032b5e5f41a56940251213ccd54e3
283kkc <pictures1682@gmail.com>
2019-02-18 02:03:36 +0000

    修正２

--- db/migrate/20190202144125_devise_create_users.rb
+++ db/migrate/20190202144125_devise_create_users.rb
@@ -36,8 +36,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :first_name, null: false, default: ""
       t.string :last_name, null: false, default: ""
       t.references :affiliation, foreign_key: true
-      t.integer :status, null: false, default: 0
       t.string :avatar, null: false, default: ""
+      t.boolean :admin, null: false, default: false
 
       t.timestamps
     end
--- db/migrate/20190217053647_add_cord_to_affiliations.rb
+++
@@ -1,5 +0,0 @@
-class AddCordToAffiliations < ActiveRecord::Migration[5.2]
-  def change
-    add_column :affiliations, :cord, :string, null: false
-  end
-end

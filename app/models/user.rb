0cbe45e5dfbd70c40fc0f903bcb38411d0f1468e
283kkc <pictures1682@gmail.com>
2019-02-18 12:09:46 +0000

    experimentの作成中。

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,10 @@ class User < ApplicationRecord
          
   belongs_to :affiliation
   has_many :experiments, dependent: :destroy
+<<<<<<< HEAD
   has_many :comments, dependent: :destroy
+=======
+>>>>>>> experimentの作成中。
          
   validates :first_name, presence: true
   validates :last_name, presence: true

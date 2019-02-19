664ae19b66bedbb54837280105e988af5ad1c2e1
283kkc <pictures1682@gmail.com>
2019-02-19 07:25:26 +0000

    作成途中２

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
+<<<<<<< HEAD
     validates :category, presence:true
     belongs_to :experiment
+=======
+    validates :name, presence:true
+>>>>>>> 作成途中２
 end
---
+++ db/migrate/20190203505404_create_experiments.rb
@@ -0,0 +1,16 @@
+class CreateExperiments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :experiments do |t|
+      t.string :title, null: false, default: ""
+      t.date :date, null: false
+      t.text :overview, null: false
+      t.text :result, null: false
+      t.references :category, foreign_key: true
+      t.references :protocol, foreign_key: true
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+    #add_index :experiments, [:user_id, :created_at]
+  end
+end

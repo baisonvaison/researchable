5feb27a31faaefbe5d9370e9ce6c8bb7fc7215f2
283kkc <pictures1682@gmail.com>
2019-02-19 07:25:26 +0000

    作成途中２

---
+++ db/migrate/20190203005759_create_affiliations.rb
@@ -0,0 +1,17 @@
+class CreateAffiliations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :affiliations do |t|
+      t.string :university, null: false, default: ""
+      t.string :department, null: false, default: ""
+      t.string :course, null: false, default: ""
+      t.string :labo, null: false, default: ""
+      t.string :cord, null: false, default: ""
+
+      t.timestamps
+    end
+
+    # 以下の記述は本来はあったほうが良いものの、 "Mysql2::Error: Specified key was too long; max key length is 1000 bytes" が殺しに来るため、いったんは保留。
+    # add_index :affiliations, [:university, :department, :course, :labo], unique: true, name: 'affiliations_index'
+
+  end
+end
---
+++ db/migrate/20190203202240_create_protocols.rb
@@ -0,0 +1,13 @@
+class CreateProtocols < ActiveRecord::Migration[5.2]
+  def change
+    create_table :protocols do |t|
+      t.references :affiliation, foreign_key: true
+      t.references :user, foreign_key: true
+      t.integer :status, null: false, default: 0
+      t.integer :parent_id, null: false, default: 0
+      t.string :title, null: false, default: ""
+
+      t.timestamps
+    end
+  end
+end
